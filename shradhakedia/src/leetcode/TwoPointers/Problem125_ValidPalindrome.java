7bd225c79b688ae3bc342f3bfa186297ae72e805
shradhakedia <shradhakedia1998@gmail.com>
2022-01-11 09:25:45 +0530

    shradhakedia: add leetcode solution 125

---
+++ shradhakedia/src/leetcode/TwoPointers/Problem125_ValidPalindrome.java
@@ -0,0 +1,34 @@
+/**
+ * Author : Sradha Kedia
+ * User   : ShradhaKedia
+ * Link   : https://leetcode.com/problems/valid-palindrome/
+ * Difficulty level: Easy
+ */
+
+package leetcode.TwoPointers;
+
+public class Problem125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        for(int i = 0, j = s.length() - 1; i < j; ) {
+
+            char leftChar = s.charAt(i);
+            char rightChar = s.charAt(j);
+            if(!(leftChar >= 'a' && leftChar <= 'z') && !(leftChar >= '0' && leftChar <= '9')) {
+                i++;
+            }
+            if(!(rightChar >= 'a' && rightChar <= 'z') && !(rightChar >= '0' && rightChar <= '9'))             {
+                j--;
+            }
+            if(((leftChar >= 'a' && leftChar <= 'z') || (leftChar >= '0' && leftChar <= '9')) && ((rightChar >= 'a' && rightChar <= 'z') || (rightChar >= '0' && rightChar <= '9')))             {
+                if(leftChar != rightChar) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
