63dd9e8c96c680aeb7f448d3253cc5cbf63ccbcd
shradhakedia <shradhakedia1998@gmail.com>
2022-01-19 00:01:41 +0530

    ShradhaKedia: add leetcode solution 27

--- shradhakedia/leetcode/TwoPointers/Problem27_RemoveElement.java
+++ shradhakedia/leetcode/TwoPointers/Problem27_RemoveElement.java
@@ -12,7 +12,7 @@ public class Problem27_RemoveElement {
     public int removeElement(int[] nums, int val) {
 
         /*
-        // Approach 1: two pointers
+        // Approach 1: Two Pointers
         int i, j;
         for(i = 0, j = nums.length - 1; i <= j; ) {
             if(nums[i] == val) {
@@ -28,7 +28,7 @@ public class Problem27_RemoveElement {
         return i;
         */
 
-        // Approach 2: two pointers
+        // Approach 2: Brute Force
         int k = 0;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != val) {
@@ -38,3 +38,8 @@ public class Problem27_RemoveElement {
         return k;
     }
 }
+
+/**
+ * Approach 1: Two Pointers; Time Complexity: O(n), Space Complexity: O(1)
+ * Approach 2: Brute Force; Time Complexity: O(n), Space Complexity: O(1)
+ */
