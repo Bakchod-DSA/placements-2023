b36e40ce86236fff6e496211dbf7bf82794479cb
nidhi <vividsky09@gmail.com>
2022-02-03 23:53:20 +0530

    solution to leetcode problem 779

---
+++ vividsky/LeetCode/recursion/problem779_KthSymbolInGrammar.java
@@ -0,0 +1,24 @@
+package LeetCode.recursion;
+/**
+ * Author : Nidhi Sangwan
+ * User   : vividSky
+ * Link   : https://leetcode.com/problems/k-th-symbol-in-grammar/
+ * Difficulty level : Medium
+ */
+public class problem779_KthSymbolInGrammar {
+
+    public int kthGrammar(int n, int k) {
+        return approachOne(n, k);
+    }
+
+    private int approachOne(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        if (k <= (1 << (n - 2))) {
+            return approachOne(n - 1, k);
+        } else {
+            return (approachOne(n - 1, k - ((1 << (n - 2))))) ^ 1;
+        }
+    }
+}
