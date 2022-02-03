a387053f847abea1133bcf69a4af68e2da677c56
nidhi <vividsky09@gmail.com>
2022-02-04 04:49:27 +0530

    add solution to leetcode problem 39

---
+++ vividsky/LeetCode/recursion/problem39_CombinationSum.java
@@ -0,0 +1,31 @@
+package LeetCode.recursion;
+/**
+ * Author : Nidhi Sangwan
+ * User   : vividSky
+ * Link   : https://leetcode.com/problems/combination-sum/
+ * Difficulty level : Medium
+ */
+public class problem39_CombinationSum {
+    List<List<Integer>> r = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        combinations(candidates, target, 0, 0, new ArrayList<Integer>());
+        return r;
+    }
+
+    private void combinations(int[] candidates, int target, int currIdx, int currSum, List<Integer> ans) {
+
+        if (currSum == target) {
+            r.add(new ArrayList<>(ans));
+            return;
+        }
+
+        for (int i = currIdx; i < candidates.length; i++) {
+            if (candidates[i] + currSum <= target) {
+                ans.add(candidates[i]);
+                combinations(candidates, target, i, currSum + candidates[i], ans);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+}
