efae84d7ebc0f6d42139e6d9c5d0f81678f3d8d1
shradhakedia <shradhakedia1998@gmail.com>
2022-02-05 19:26:00 +0530

    ShradhaKedia: add coding blocks Solution

---
+++ shradhakedia/CodingBlocks_plus_AdityaVermaSheet/Recursion/PermutationWithSpaces.java
@@ -0,0 +1,36 @@
+/**
+ * Author : Sradha Kedia
+ * Link   : https://www.youtube.com/watch?v=1cspuQ6qHW0&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=14
+ * Difficulty level : Medium
+ */
+
+package CodingBlocks_plus_AdityaVermaSheet.Recursion;
+
+import java.util.*;
+
+public class PermutationWithSpaces {
+
+    public static void main(String[] args) {
+
+        System.out.println(permuteSpace("abcd"));
+    }
+
+    private static List<String> permuteSpace(String s) {
+
+        if(s.length() == 1) {
+            List<String> baseList = new ArrayList<>();
+            baseList.add(Character.toString(s.charAt(0)));
+            return baseList;
+        }
+
+        List<String> recurList = permuteSpace(s.substring(1));
+        List<String> myList = new ArrayList<>();
+        for(String rs : recurList) {
+            myList.add(s.charAt(0) + rs);
+            myList.add(s.charAt(0) + "_" + rs);
+        }
+
+        return myList;
+    }
+
+}
