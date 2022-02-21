78d08da38fe6eb8988bb242b9bcea8ce7775104c
shradhakedia <shradhakedia1998@gmail.com>
2022-02-21 15:19:51 +0530

    ShradhaKedia: add Aditya Verma playlist solution

---
+++ shradhakedia/CodingBlocks_plus_AdityaVermaSheet/Heap/SortANearlySortedArray.java
@@ -0,0 +1,36 @@
+package CodingBlocks_plus_AdityaVermaSheet.Heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class SortANearlySortedArray {
+
+    public static void main(String[] args) {
+
+        int[] result = sort(new int[]{6, 5, 3, 2, 8, 10, 9}, 3);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] sort(int[] nums, int k) {
+
+        int[] result = new int[nums.length];
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        int index = 0;
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                result[index++] = minHeap.poll();
+            }
+        }
+
+        while (!minHeap.isEmpty()) {
+            result[index++] = minHeap.poll();
+        }
+
+        return result;
+    }
+
+
+}
