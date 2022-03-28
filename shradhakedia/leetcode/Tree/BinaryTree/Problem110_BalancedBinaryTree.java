794552b0573e9a568d5bd4e36afc63cd37924fe1
shradhakedia <shradhakedia1998@gmail.com>
2022-03-28 11:39:11 +0530

    ShradhaKedia: add leetcode solution 110

---
+++ shradhakedia/leetcode/Tree/BinaryTree/Problem110_BalancedBinaryTree.java
@@ -0,0 +1,50 @@
+/**
+ * Author : Sradha Kedia
+ * User   : ShradhaKedia
+ * Link   : https://leetcode.com/problems/balanced-binary-tree/
+ * Difficulty level: Easy
+ */
+
+package leetcode.Tree.BinaryTree;
+
+// a binary tree node.
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class Problem110_BalancedBinaryTree {
+
+    public boolean result = true;
+
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return result;
+    }
+
+    private int height(TreeNode root) {
+        /*  Approach: dfs
+            Time Complexity: O(n)
+            Space Complexity: O(h), height of the tree due to recursive stack
+        */
+
+        if(root == null) {
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left - right) > 1) {
+            result = false;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+}
