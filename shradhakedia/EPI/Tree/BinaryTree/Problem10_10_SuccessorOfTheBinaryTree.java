695d687029da05c19b84d18e8bf13a20dac793ae
shradhakedia <shradhakedia1998@gmail.com>
2022-03-31 16:14:53 +0530

    ShradhaKedia: add EPI solution 10.10

---
+++ shradhakedia/EPI/Tree/BinaryTree/Problem10_10_SuccessorOfTheBinaryTree.java
@@ -0,0 +1,38 @@
+/**
+ * Author : Sradha Kedia
+ * Page no.: 163, 164 of Epi Java
+ * Problem no.: 10.10
+ * Difficulty level : Medium
+ */
+
+package EPI.Tree.BinaryTree;
+
+public class Problem10_10_SuccessorOfTheBinaryTree {
+
+//    public static BinaryTree<Integer> findSuccessor(BinaryTree<Integer> node) {
+//        BinaryTree<Integer> curr = node;
+//        if(curr == null) {
+//            return null;
+//        }
+//
+//        if(curr.right != null) {
+//            curr = curr.right;
+//            while(curr.left != null) {
+//                curr = curr.left;
+//            }
+//            return curr;
+//        }
+//
+//        BinaryTree<Integer> temp = curr;
+//        curr = curr.parent;
+//        while(curr != null && curr.left != temp) {
+//            temp = curr;
+//            curr = curr.parent;
+//
+//        }
+//
+//        return curr;
+//
+//    }
+//
+}
