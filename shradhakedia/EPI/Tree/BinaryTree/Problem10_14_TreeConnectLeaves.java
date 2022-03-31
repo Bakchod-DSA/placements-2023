29480d9e2af5b4f0767592a5dd81d492a182fb07
shradhakedia <shradhakedia1998@gmail.com>
2022-03-31 16:15:12 +0530

    ShradhaKedia: add EPI solution 10.14

---
+++ shradhakedia/EPI/Tree/BinaryTree/Problem10_14_TreeConnectLeaves.java
@@ -0,0 +1,38 @@
+/**
+ * Author : Sradha Kedia
+ * Page no.: 168, 169 of Epi Java
+ * Problem no.: 10.14
+ * Difficulty level : Medium
+ */
+
+package EPI.Tree.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem10_14_TreeConnectLeaves {
+
+//    public static List<BinaryTreeNode<Integer>> createListOfLeaves(BinaryTreeNode<Integer> tree) {
+//
+//        /*  Approach: preorder
+//            Time Complexity: O(n)
+//            Space Complexity: O(n)
+//        */
+//
+//        List<BinaryTreeNode<Integer>> leaves = new LinkedList<>();
+//        preorder(tree, leaves);
+//        return leaves;
+//    }
+//
+//    private static void preorder(BinaryTreeNode<Integer> root, List<BinaryTreeNode<Integer>> leaves) {
+//        if(root == null) {
+//            return;
+//        }
+//        if(root.left == null && root.right == null) {
+//            leaves.add(root);
+//        }
+//        preorder(root.left, leaves);
+//        preorder(root.right, leaves);
+//
+//    }
+}
