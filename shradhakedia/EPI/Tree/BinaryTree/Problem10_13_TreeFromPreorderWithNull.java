64012de6baa6af485c15bbc8cb6e9e9358eefa5a
shradhakedia <shradhakedia1998@gmail.com>
2022-03-31 16:15:03 +0530

    ShradhaKedia: add EPI solution 10.13

---
+++ shradhakedia/EPI/Tree/BinaryTree/Problem10_13_TreeFromPreorderWithNull.java
@@ -0,0 +1,34 @@
+/**
+ * Author : Sradha Kedia
+ * Page no.: 167, 168 of Epi Java
+ * Problem no.: 10.13
+ * Difficulty level : Medium
+ */
+
+package EPI.Tree.BinaryTree;
+
+import java.util.List;
+
+public class Problem10_13_TreeFromPreorderWithNull {
+
+    private static int index;
+
+//    public static BinaryTreeNode<Integer> reconstructPreorder(List<Integer> preorder) {
+//        index = 0;
+//        return reconstructTreeFromPreorder(preorder);
+//    }
+//
+//    private static BinaryTreeNode<Integer> reconstructTreeFromPreorder(List<Integer> preorder) {
+//
+//        if(preorder.get(index) == null) {
+//            index++;
+//            return null;
+//        }
+//
+//        Integer key = preorder.get(index++);
+//        return new BinaryTreeNode<>(key, reconstructTreeFromPreorder(preorder), reconstructTreeFromPreorder(preorder));
+//
+//
+//    }
+
+}
