6909424e8d57d25c30304b6ba8eaed684f24b8ab
shradhakedia <shradhakedia1998@gmail.com>
2022-04-05 18:58:01 +0530

    modify leetcode solution 1008

--- shradhakedia/leetcode/Tree/BinarySearchTree/Problem1008_ConstructBSTFromPreorder.java
+++ shradhakedia/leetcode/Tree/BinarySearchTree/Problem1008_ConstructBSTFromPreorder.java
@@ -13,18 +13,42 @@ public class Problem1008_ConstructBSTFromPreorder {
 
     public TreeNode bstFromPreorder(int[] preorder) {
 
-        return constructBst(preorder, Integer.MAX_VALUE);
+        // return constructBst(preorder, Integer.MAX_VALUE);
+        return bruteForce(preorder, 0, preorder.length);
     }
 
-    private TreeNode constructBst(int[] preorder, int prev) {
-        if(index == preorder.length || preorder[index] > prev) {
+    private TreeNode constructBst(int[] preorder, int bound) {
+        /*  Approach: Optimised
+            Time Complexity: O(n)
+            Space Complexity: O(h)
+        */
+
+        if(index == preorder.length || preorder[index] > bound) {
             return null;
         }
 
         TreeNode node = new TreeNode(preorder[index++]);
         node.left = constructBst(preorder, node.val);
-        node.right = constructBst(preorder, prev);
+        node.right = constructBst(preorder, bound);
 
         return node;
+    }
+
+    private TreeNode bruteForce(int[] preorder, int start, int end) {
+        /*  Approach: brute Force
+            Time Complexity: O(n ^ 2) worst, O(n logn) average, O(n) best.
+            Space Complexity: O(h)
+        */
+
+        if(start >= end) {
+            return null;
+        }
+
+        int transitionPoint = start + 1;
+        while(transitionPoint < end && preorder[transitionPoint] < preorder[start]) {
+            transitionPoint++;
+        }
+
+        return new TreeNode(preorder[start], bruteForce(preorder, start + 1, transitionPoint), bruteForce(preorder, transitionPoint, end));
     }
 }
