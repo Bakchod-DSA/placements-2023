318ac19d2b79a55dd4029ff68b02d39eed73f2d6
shradhakedia <shradhakedia1998@gmail.com>
2022-05-27 23:49:46 +0530

    add leetcode solution 341

---
+++ shradhakedia/leetcode/Stacks/Problem341_FlattenNestedListIterator.java
@@ -0,0 +1,67 @@
+/**
+ * Author : Sradha Kedia
+ * User   : ShradhaKedia
+ * Link   : https://leetcode.com/problems/flatten-nested-list-iterator/
+ * Difficulty level: Medium
+ */
+package leetcode.Stacks;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem341_FlattenNestedListIterator {
+
+    public interface NestedInteger {
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        boolean isInteger();
+        // @return the single integer that this NestedInteger holds, if it holds a single integer. Return null if this NestedInteger holds a nested list
+        Integer getInteger();
+        // @return the nested list that this NestedInteger holds, if it holds a nested list. Return empty list if this NestedInteger holds a single integer
+        List<NestedInteger> getList();
+    }
+
+    public static class NestedIterator implements Iterator<Integer> {
+
+        Deque<NestedInteger> stack;
+        public NestedIterator(List<NestedInteger> nestedList) {
+            stack = new LinkedList<>();
+            buildStack(nestedList);
+        }
+
+        @Override
+        public Integer next() {
+            if(!hasNext()) {
+                return null;
+            }
+            return stack.removeFirst().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while(!stack.isEmpty()) {
+                NestedInteger ni = stack.peekFirst();
+                if(!ni.isInteger()) {
+                    stack.removeFirst();
+                    buildStack(ni.getList());
+                } else {
+                    break;
+                }
+            }
+            return !stack.isEmpty();
+        }
+
+        private void buildStack(List<NestedInteger> nestedList) {
+            for(int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.addFirst(nestedList.get(i));
+            }
+        }
+    }
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+}
